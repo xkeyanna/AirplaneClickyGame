581a2287e6450deceab6acb9cab8685c69b6d9a3
xkeekyx <xkeyanna@yahoo.com>
2019-03-28 11:49:15 -0700

    new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from ".components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import FriendCard from "./components/FriendCard";
+import Footer from "./components/Footer";
+import airplanes from "./airplanes.json";
+import "./App.css";
 
+// sets state to 0 or empty
 class App extends Component {
+  state = {
+    airplanes,
+    clickedAirplane: [],
+    score: 0
+  };
+
+  // when you click on a card... the airplane is taken out of the array
+  imageClick = event => {
+    const currentAirplane = event.target.addEventListener;
+    const AirplaneAlreadyClicked = this.state.clickedAirplane.indexOf(currentAirplane) > -1;
+
+    // if you click on an airplane that has already been selected, the game is reset and cards reordered
+    if (AirplaneAlreadyClicked) {
+      this.setState({
+        airplane: this.state.airplane.sort(function (a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedAirplane: [],
+        score: 0
+      });
+      alert("You lose. Play again?");
+      // if you click on an available fish, your score is increased and cards reordered
+    } else {
+      this.setState({
+        airplane: this.state.airplane.sort(function (a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedAirplane: this.state.clickedAirplane.concat(
+          currentAirplane
+        ),
+        score: this.state.score + 1
+      },
+        //if you get all 12 airplanes correct you get a congrats message and the game resets
+        () => {
+          if (this.state.score === 12) {
+            alert("Yay! You Win!");
+            this.setState({
+              airplanes: this.state.airplane.sort(function (a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedAirplane: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+  //the reorder of components to be rendered: navbar, jumbotron, friendcard, footer
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar score={this.state.score}
+        />
+        <Jumbotron />
+        <div className="wrapper">
+          {this.state.fish.map(fish => (
+            <FriendCard
+              imageClick={this.imageClick}
+              id={airplanes.id}
+              key={airplanes.id}
+              image={airplanes.image}
+            />
+          ))}
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>Game by Keyanna Xu</p>
+	</header>
+);
+
+export default Footer;
